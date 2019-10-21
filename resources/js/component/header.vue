e140888072c7c6a4ba425bc9e9bd79197cb6dc88
Sumona Choity <choity.sumona@gmail.com>
2019-10-21 13:03:31 +0600

    from choity pc

--- resources/js/component/header.vue
+++ resources/js/component/header.vue
@@ -1,49 +1,51 @@
 <template>
-    <div >
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <router-link to="/" class="nav-link"   >
-                        Home <span class="sr-only">(current)</span>
-                    </router-link>
-                </li>
-                <li class="nav-item active">
-                    <router-link to="/add_product" class="nav-link"   >
-                        Add Product <span class="sr-only">(current)</span>
-                    </router-link>
-                </li>
-                <li class="nav-item active">
-                    <router-link to="/category" class="nav-link"   >
-                        All Category <span class="sr-only">(current)</span>
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                     <router-link to="/about" class="nav-link"   >
-                        About 
-                    </router-link>
-                </li>
-                <template v-if="isLoggedIn">
-                     <li class="nav-item">
-                        <a href="/logout"  class="nav-link" >Logout</a>
-                    </li>
-                </template>
-                <template v-else>
-                     <li class="nav-item">
-                        <router-link to="/login" class="nav-link"   >
-                            Login 
-                        </router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/registration" class="nav-link"   >
-                            Registration 
-                        </router-link>
-                    </li>
-                </template>
-               
-               
-                </ul>
+    <div class="container-fluid"  >
+        <nav class="navbar navbar-expand-lg navbar-light bg-light row">
+                <div class="collapse navbar-collapse col-md-6" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <router-link to="/" class="nav-link"   >
+                                Home <span class="sr-only">(current)</span>
+                            </router-link>
+                        </li>
+                        <li class="nav-item active">
+                            <router-link to="/add_product" class="nav-link"   >
+                                Add Product <span class="sr-only">(current)</span>
+                            </router-link>
+                        </li>
+                        <li class="nav-item active">
+                            <router-link to="/category" class="nav-link"   >
+                                All Category <span class="sr-only">(current)</span>
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                             <router-link to="/about" class="nav-link"   >
+                                About 
+                            </router-link>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-md-6">
+                    <ul class="navbar-nav menu_right">
+                        <template v-if="isLoggedIn">
+                             <li class="nav-item">
+                                <a href="/logout"  class="nav-link" >Logout</a>
+                            </li>
+                        </template>
+                        <template v-else>
+                             <li class="nav-item">
+                                <router-link to="/login" class="nav-link"   >
+                                    Login 
+                                </router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link to="/registration" class="nav-link"   >
+                                    Registration 
+                                </router-link>
+                            </li>
+                        </template>
+                    </ul>
             </div>
         </nav>
     </div>
@@ -60,3 +62,11 @@ export default {
 
 }
 </script>
+
+<style>
+    .menu_right{
+        width: 100%;
+         
+         justify-content: flex-end;
+    }
+</style>
